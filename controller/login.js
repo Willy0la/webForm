285b6872b24e2c786373bd2fb30d64e9b65075d1
Willy0la <132810663+Willy0la@users.noreply.github.com>
2025-02-06 23:27:46 +0100

    simple change

--- controller/login.js
+++ controller/login.js
@@ -5,24 +5,26 @@ import bcrypt from "bcryptjs";
 import User from "../model/user.js";
 
 const login =  async (req, res, next) => {
-    const { email, password } = req.body;
+    const { email, password, userName } = req.body;
   
     if (!email || !password) {
       return res.status(400).send("Kindly input your email and password");
     }
   
     try {
-      const user = await User.findOne({ email });
+      const user = await User.findOne({$or:[{ email, userName}] });
       if (!user) {
         return res.status(400).send("User not found.");
       }
   
-        const checkPassword = await bcrypt.compare(password, user.password);
-        if (!checkPassword) {
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
           return res.status(400).send("incorrect password");
         }
   
-        res.status(200).send({ message: "Login successful!" });
+        res.status(200).send({ message: "Login successful!" ,
+          email:user.email , username: user.userName
+        });
      
     } catch {
       res.status(500).send("Something went wrong. Please try again later.");
--- model/user.js
+++ model/user.js
@@ -6,6 +6,7 @@ import mongoose from "mongoose";
 const userSchema = new mongoose.Schema({
   firstName: { type: String, required: true },  
   lastName: { type: String, required: true },
+  userName: { type: String, required: true },
   email: { type: String, required: true, unique: true },  
   password: { type: String, required: true, minlength: 8 },
 });
